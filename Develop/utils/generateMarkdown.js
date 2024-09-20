f7b48ad0723d1f1694a31124065d27c3f7684a30
Black Magic <dlyoungworking@gmail.com>
2024-09-19 22:32:02 -0400

    NodeJS Challenge

---
+++ Develop/utils/generateMarkdown.js
@@ -0,0 +1,44 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+    if (license === 'MIT') {
+        return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+    } else if (license === 'GNU') {
+        return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+    } else {
+        return '';
+    }
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+    if (license === 'MIT') {
+        return `[MIT](https://opensource.org/licenses/MIT)`;
+    } else if (license === 'GNU') {
+        return `[GNU](https://www.gnu.org/licenses/gpl-3.0)`;
+    } else {
+        return '';
+    }
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+    if (license === 'MIT') {
+        return `This project is licensed under the [MIT](https://opensource.org/licenses/MIT) license.`;
+    } else if (license === 'GNU') {
+        return `This project is licensed under the [GNU](https://www.gnu.org/licenses/gpl-3.0) license.`;
+    } else {
+        return '';
+    }
+}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+  
+`;
+}
+
+export default generateMarkdown;
